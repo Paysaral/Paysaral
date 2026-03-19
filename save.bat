d591b5001d82324b62d1b3041a84a40dd25556cc
Paysaral <paysaral24@gmail.com>
2026-03-19 19:09:30 +0530

    test save

---
+++ save.bat
@@ -0,0 +1,6 @@
+@echo off
+git add .
+git commit -m "Auto Save"
+git push https://Paysaral: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@github.com/Paysaral/Paysaral.git master
+echo Done! Code Saved to GitHub!
+pause
